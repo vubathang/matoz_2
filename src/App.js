68bbf10e4b2c0c7c42af72862a4245c2c7dbbc32
vbthang <bathangvu@gmail.com>
2022-12-05 20:49:20 +0700

    design matoz dashboard

--- src/App.js
+++ src/App.js
@@ -3,47 +3,96 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* HEADER */}
+      <div class="grid-container">
+        {/* HEADER */}
         <header class="header">
+          <div clas="header-left"></div>
           <div class="header-right">
             <span class="material-symbols-outlined">group</span>
             <span class="material-symbols-outlined">notifications</span>
             <span class="material-symbols-outlined">account_circle</span>
           </div>
         </header>
-      {/* END HEADER */}
+        {/* END HEADER */}
 
-      {/* SIDEBAR */}
-      <aside id="sidebar">
-        <div class="sidebar-title">
-          <div class="sidebar-brand font24">
-            <span class="material-symbols-outlined" >emoji_objects</span> MATOZ
+        {/* SIDEBAR */}
+        <aside id="sidebar">
+          <div class="sidebar-title">
+            <div class="sidebar-brand font24">
+              <span class="material-symbols-outlined" >emoji_objects</span> MATOZ
+            </div>
+            <span class="material-symbols-outlined" onclick="closeSidebar()">close</span>
           </div>
-          <span class="material-symbols-outlined" onclick="closeSidebar()">close</span>
-        </div>
-        <ul class="sidebar-list">
-          <li class="sidebar-list-item">
-            <span class="material-symbols-outlined">home</span>Home
-          </li>
-          <li class="sidebar-list-item">
-            <span class="material-symbols-outlined">signal_cellular_alt</span>Gantt Chart        
-          </li>
-          <li class="sidebar-list-item">
-            <span class="material-symbols-outlined">settings</span>Settings
-          </li>
+          <ul class="sidebar-list">
+            <li class="sidebar-list-item">
+              <span class="material-symbols-outlined">home</span>Home
+            </li>
+            <li class="sidebar-list-item">
+              <span class="material-symbols-outlined">signal_cellular_alt</span>Gantt Chart        
+            </li>
+            <li class="sidebar-list-item">
+              <span class="material-symbols-outlined">settings</span>Settings
+            </li>
+          </ul>
+        </aside>
+        {/* END SIDEBAR */}
 
-        </ul>
-      </aside>
-      {/* END SIDEBAR */}
+        {/* MAIN */}
+        <main class="main-container">
+          <div class="list-columns">
+            {/* <p class="font-weight-bold font24">WORKSPACES</p> */}
+            <div class="list">
+              <header>List 1</header>
+              <ul>
+                <li>Em đã yêu một người có ước mơ</li>
+                <li>Mơ điều làm em thấy hoang mang lo sợ</li>
+                <li>Sợ yêu một người không phải như em đã từng</li>
+                <li>Nễu như anh đi về phía đó</li>
+                <li>Liệu rằng anh có còn thấy em</li>
+                <li>Lệ trong đôi mắt sao mà lấp đi ánh nắng mặt trời</li>
+                <li>Vẫn yêu dù cho có cô đơn</li>
+                <li>Dù cho anh không của riêng em sau này</li>
+              </ul>
+              <footer>Add another task</footer>
+            </div>
+            <div class="list">
+              <header>List 1</header>
+              <ul>
+                <li>Em đã yêu một người có ước mơ</li>
+                <li>Mơ điều làm em thấy hoang mang lo sợ</li>
+                <li>Sợ yêu một người không phải như em đã từng</li>
+                <li>Nễu như anh đi về phía đó</li>
+                <li>Liệu rằng anh có còn thấy em</li>
+              </ul>
+              <footer>Add another task</footer>
+            </div>
+            <div class="list">
+              <header>List 1</header>
+              <ul>
+                <li>Em đã yêu một người có ước mơ</li>
+                <li>Mơ điều làm em thấy hoang mang lo sợ</li>
+                <li>Sợ yêu một người không phải như em đã từng</li>
+                <li>Nễu như anh đi về phía đó</li>
+                <li>Liệu rằng anh có còn thấy em</li>
+              </ul>
+              <footer>Add another task</footer>
+            </div>
+            <div class="list">
+              <header>List 1</header>
+              <ul>
+                <li>Em đã yêu một người có ước mơ</li>
+                <li>Mơ điều làm em thấy hoang mang lo sợ</li>
+                <li>Sợ yêu một người không phải như em đã từng</li>
+                <li>Nễu như anh đi về phía đó</li>
+                <li>Liệu rằng anh có còn thấy em</li>
+              </ul>
+              <footer>Add another task</footer>
+            </div>
+          </div>
+        </main>
+        {/* END MAIN */}
+      </div>
 
-      {/* MAIN */}
-      <main class="main-container">
-        <div class="main-title">
-          <p class="font-weight-bold font24">WORKSPACES</p>
-          <div class=""></div>
-        </div>
-      </main>
-      {/* END MAIN */}
     </div>
   );
 }
