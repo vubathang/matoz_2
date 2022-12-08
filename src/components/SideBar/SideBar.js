28629ca284d1c69668f3c8f22716b13471683d6b
vbthang <bathangvu@gmail.com>
2022-12-08 23:58:22 +0700

    change color

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -7,19 +7,19 @@ function SideBar() {
     <aside id="sidebar">
       <div class="sidebar-title">
         <div class="sidebar-brand font24">
-          <span class="material-symbols-outlined" >emoji_objects</span> MATOZ
+          <span class="material-symbols-outlined color_light" >emoji_objects</span> MATOZ
         </div>
         <span class="material-symbols-outlined" onclick="closeSidebar()">close</span>
       </div>
       <ul class="sidebar-list">
         <li class="sidebar-list-item">
-          <span class="material-symbols-outlined">home</span>Home
+          <span class="material-symbols-outlined home">home</span>Home
         </li>
         <li class="sidebar-list-item">
-          <span class="material-symbols-outlined">signal_cellular_alt</span>Gantt Chart        
+          <span class="material-symbols-outlined grandient">signal_cellular_alt</span>Gantt Chart        
         </li>
         <li class="sidebar-list-item">
-          <span class="material-symbols-outlined">settings</span>Settings
+          <span class="material-symbols-outlined white">settings</span>Settings
         </li>
       </ul>
     </aside>
